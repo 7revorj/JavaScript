495e5b66474d14a20f496fe951b27b52ae439890
trevor jelinek <trevorjelinek@gmail.com>
2023-06-28 17:04:34 -0400

    Several JS functions

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -9,7 +9,7 @@ var U = "Warning, warning, you are entering an uncharted sector of deep space"; 
 window.alert(U) //Windows Alerts with the value of U which is "Hello"
 
 document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"<br> \"Eat my shorts!\" Bart responded."); //Writes the string and uses \ to ignore ' and ""
-                                                                    // <br> breaks the line but can only do so within a string unless it is in the html
+// <br> breaks the line but can only do so within a string unless it is in the html
 document.write(" Don\'t you dare touch my shorts!") //writes the string
 
 document.write("\"Be who you are and say what you feel,"
@@ -20,7 +20,7 @@ document.write("This is a concatenated" //Same as above
     + " string");
 
 var ZZZ = "<br>Oh boy" + " I'm tired" //assigns the variable ZZZ a line break and concatenated string
-document.write(ZZZ); 
+document.write(ZZZ);
 
 var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", //Assigns the variables Family, Dad, Mom
     Daughter = "Penny", Son = "Zorro"; //assigns the variables Son, Daughter
@@ -36,15 +36,25 @@ document.write(9 / 3); //An expression that divides 9 by 3 and writes the result
 
 function My_First_Function() {                  //Defining a function and allowing it to have an anonymous name
     var str = "This is the button text!";      //Defining a variable and giving it
-                                                // a string value
+                                              // a string value
     document.getElementById("Button_Text").innerHTML = str; //Putting the value
-                                                    //with the "Button_Text" id 
+                                              //with the "Button_Text" id 
 }
 
 window.onload = function (onload) {                    //Window.onload is an event that calls the function (which has been named onload) AFTER the rest of the page has loaded
     var Sent1 = "This is the beginning of the string"; //Creates a variable Sent1 and assigns it the value "This is the beginning of the string"
     var Sent2 = "This is the end of the string<br>";   //Creates a variable Sent2 and assigns it the value "This is the end of the string<br>"
     var combinedSent = Sent1 + ". " + Sent2;            //Creates a variable combinedSent and assigns it the value Sent1 + ". " + Sent2
-    document.getElementById("myElement").innerHTML = combinedSent; //Assigns the myElement id the combinedSent variable
+    document.getElementById("myElement").innerHTML = combinedSent; //Assigns the myElement id the combinedSent variable and SETS the HTML content of the myElement id 
+
+                                                                //The above SETS the HTML content of the myElement id
+                                                                //The below GETS the HTML content of the myElement id (which is now the result of combinedSent)
+    
+    var elementContent = document.getElementById("myElement").innerHTML; //Creates a variable elementContent and assigns it the value of the myElement id   
+    document.getElementById("demo").innerHTML = elementContent; // Logs the value of elementContent
+    
+                                                                // Below is redundant code to demonstrate that the most recent variable receives precedence.
+    var elementContent = document.getElementById("myElement").innerHTML;
+    document.getElementById("demo").innerHTML = "Blah";
 }
 
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,26 @@
+
+
+function changeTime() {  //This function calls for the current time and sets the value of the ID time
+    currentTime = new Date().toLocaleTimeString(); // Update the global currentTime variable
+    document.getElementById('Time').innerHTML = currentTime; // Replaces the value of the element ID Time with the result of the element currentTIme
+}
+
+function changeDate() {  //This function calls for the current Date and sets the value of the ID date
+    currentDate = new Date().toLocaleDateString(); // Update the global currentDate variable
+    document.getElementById('Date').innerHTML = currentDate; // Replaces the value of the element ID Date with the result of the element currentDate
+}
+currentDate = new Date().toLocaleDateString(); // Update the global currentDate variable with the current Date
+currentTime = new Date().toLocaleTimeString(); // Update the global currentTime variable with the current Time
+
+function myFunction() {                        // This function sets the value of timeDate to the current time and date
+    var Time = currentTime + " " + currentDate; // Creates a variable Time and assigns it the value currentTime + " " + currentDate
+    document.getElementById('timeDate').innerHTML = Time; // Replaces the value of the element ID timeDate with the result of the element Time
+}
+
+function myFunction2() {                        // This function sets the value of Concatenate to the current time and date
+    var sentence = "I am learning";             // Creates a variable sentence and assigns it the value "I am learning"
+    sentence += " a lot from this JS course!";  // Adds the value " a lot from this JS course!"
+    document.getElementById("Concatenate").innerHTML = sentence;  // Replaces the value of the element ID Concatenate with the result of the element sentence
+}
+
+
